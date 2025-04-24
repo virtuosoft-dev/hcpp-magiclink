104bdba8622f80e892df9287e762cbf1620dca37
Stephen J. Carnam <steveorevo@gmail.com>
2025-04-23 19:50:31 -0700

    wip

--- magiclink.php
+++ magiclink.php
@@ -12,7 +12,212 @@
 
  if ( ! class_exists( 'MagicLink' ) ) {
     class MagicLink extends HCPP_Hooks {
+        public $magiclink_folder = "/usr/local/hestia/data/hcpp/magiclink";
+        /**
+         * Append the Magic Link checkbox and URL input field to the web form
+         */
+        public function hcpp_edit_web_xpath( $xpath ) {
+            global $hcpp;
 
+            // Add "Enable Magic Link" checkbox and "Magic Link URL" textbox
+            $checkboxNode = $xpath->query('//div[@class="form-check u-mb10"][input[@id="v-redirect-checkbox"]]')->item(0);
+            if ($checkboxNode) {
+                $doc = $xpath->document; // Get the DOMDocument associated with the DOMXPath
+
+                // Create the parent container div with x-data attribute
+                $parentDiv = $doc->createElement('div');
+                $parentDiv->setAttribute('x-data', '{ magicLinkEnabled: false }');
+
+                // Create the checkbox div
+                $checkboxDiv = $doc->createElement('div');
+                $checkboxDiv->setAttribute('class', 'form-check u-mb10');
+
+                // Create the checkbox input
+                $checkboxInput = $doc->createElement('input');
+                $checkboxInput->setAttribute('x-model', 'magicLinkEnabled');
+                $checkboxInput->setAttribute('class', 'form-check-input');
+                $checkboxInput->setAttribute('type', 'checkbox');
+                $checkboxInput->setAttribute('name', 'v-magiclink-checkbox');
+                $checkboxInput->setAttribute('id', 'v-magiclink-checkbox');
+
+                // Create the checkbox label
+                $checkboxLabel = $doc->createElement('label', 'Enable Magic Link');
+                $checkboxLabel->setAttribute('for', 'v-magiclink-checkbox');
+
+                // Append checkbox input and label to the checkbox div
+                $checkboxDiv->appendChild($checkboxInput);
+                $checkboxDiv->appendChild($checkboxLabel);
+
+                // Create the container div for Magic Link URL
+                $containerDiv = $doc->createElement('div');
+                $containerDiv->setAttribute('x-cloak', '');
+                $containerDiv->setAttribute('x-show', 'magicLinkEnabled');
+                $containerDiv->setAttribute('class', 'u-pl30 u-mb10');
+
+                // Create the inner div
+                $innerDiv = $doc->createElement('div');
+                $innerDiv->setAttribute('class', 'u-mb10');
+
+                // Create the label
+                $label = $doc->createElement('label', 'Magic Link URL');
+                $label->setAttribute('for', 'v-magiclink-url');
+                $label->setAttribute('class', 'form-label');
+
+                // Create the input
+                $input = $doc->createElement('input');
+                $input->setAttribute('type', 'text');
+                $input->setAttribute('class', 'form-control');
+                $input->setAttribute('style', 'font-family: monospace;');
+                $input->setAttribute('name', 'v-magiclink-url');
+                $input->setAttribute('id', 'v-magiclink-url');
+                $input->setAttribute('readonly', 'readonly');
+                $input->setAttribute('onclick', 'copyToClipboard(this)'); // Add onclick event
+
+                // Determine the magic link URL
+                $domain = $_GET['domain'];
+                $domain = filter_var($domain, FILTER_SANITIZE_URL);
+                $user = $_SESSION["user"];
+                if ($_SESSION["look"] != "") {
+                    $user = $_SESSION["look"];
+                }
+                $link = $hcpp->run( "v-invoke-plugin magiclink_get $user $domain" );
+                if ($link == '') {
+                    $link = 'https://' . $domain . '/' . $hcpp->random_chars( 12 );
+                }else{
+                    $link = 'https://' . $domain . '/' . $link;
+                    $parentDiv->setAttribute('x-data', '{ magicLinkEnabled: true }');
+                }
+                $input->setAttribute( 'value', $link );
+
+                // Append label and input to the inner div
+                $innerDiv->appendChild($label);
+                $innerDiv->appendChild($input);
+
+                // Append the inner div to the container div
+                $containerDiv->appendChild($innerDiv);
+
+                // Create the small text
+                $smallText = $doc->createElement('small', 'Note: click to copy to clipboard after saving form. ');
+                $learnMoreLink = $doc->createElement('a', 'Learn more');
+                $learnMoreLink->setAttribute('href', 'https://devstia.com/about-magiclink');
+                $learnMoreLink->setAttribute('target', '_blank');
+                $smallText->appendChild($learnMoreLink);
+
+                // Append the small text to the container div
+                $containerDiv->appendChild($smallText);
+
+                // Append checkbox div and container div to the parent div
+                $parentDiv->appendChild($checkboxDiv);
+                $parentDiv->appendChild($containerDiv);
+
+                // Insert the parent div into the DOM as the previous sibling
+                $checkboxNode->parentNode->insertBefore($parentDiv, $checkboxNode);
+
+                // Add JavaScript for clipboard functionality
+                $script = $doc->createElement('script', '
+                    function copyToClipboard(element) {
+                        navigator.clipboard.writeText(element.value).then(function() {
+                            alert("Copied to clipboard: " + element.value);
+                        }).catch(function(error) {
+                            console.error("Clipboard copy failed:", error);
+                        });
+                    }
+                ');
+                $doc->appendChild($script);
+            }
+
+            return $xpath;
+        }
+
+        /**
+         * Save the Magic Link URL to a file
+         */
+        public function hcpp_ob_started( $args ) {
+            global $hcpp;
+            if ( ! isset( $_REQUEST['save'] ) ||  !isset( $_REQUEST['v-magiclink-url'] ) ) return $args;
+            if ( $_REQUEST['save'] != 'save' ) return $args;
+
+            // Access session variablesariables
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+
+            // Validate token
+            if ( !isset($_SESSION['token']) || $_SESSION['token'] !== $_REQUEST['token'] ) return $args;
+
+            // Get user and domain
+            $user = $_SESSION["user"];
+            if ($_SESSION["look"] != "") {
+                $user = $_SESSION["look"];
+            }
+            $domain = $_REQUEST['v_domain'];
+            $domain = filter_var($domain, FILTER_SANITIZE_URL);
+
+            if ( isset( $_REQUEST['v-magiclink-checkbox'] ) && $_REQUEST['v-magiclink-checkbox'] == 'on' ) {
+                $link = $_REQUEST['v-magiclink-url'];
+                $link = filter_var($link, FILTER_SANITIZE_URL);
+                $link = $hcpp->getRightMost( $link, '/' );
+                $hcpp->run( "v-invoke-plugin magiclink_update $user $domain $link" );
+            } else {
+                $hcpp->run( "v-invoke-plugin magiclink_update $user $domain" );
+            }
+            return $args;
+        }
+
+        /**
+         * Process protected commands
+         */
+        public function hcpp_invoke_plugin( $args ) {
+            global $hcpp;
+
+            // Update magiclink file
+            if ( $args[0] == 'magiclink_update' ) {
+                if ( isset( $args[1] ) ) {
+                    $username = preg_replace( "/[^a-zA-Z0-9-_]+/", "", $args[1] ); // Sanitized username
+                }
+                if ( isset( $args[2] ) ) {
+                    $domain = preg_replace( "/[^a-zA-Z0-9-_.]+/", "", $args[2] ); // Allow periods in domain
+                }
+    
+                if ( !file_exists( $this->magiclink_folder ) ) {
+                    mkdir( $this->magiclink_folder, 0700, true );
+                }
+                $magiclink_file = "{$this->magiclink_folder}/{$username}_{$domain}";
+                if ( isset( $args[3] ) ) {
+                    $magiclink = preg_replace( "/[^a-zA-Z0-9-_]+/", "", $args[3] ); // Sanitized magiclink
+                }
+                if ( is_null( $magiclink ) ) {
+                    unlink ( $magiclink_file );
+                }else{
+                    file_put_contents( $magiclink_file, $magiclink );
+                    chown( $magiclink_file, $username );
+                    chgrp( $magiclink_file, $username );
+                    chmod( $magiclink_file, 0600 );
+                    $hcpp->log( "Magic Link: {$magiclink_file} created with ${magiclink}" );
+                }
+            }
+
+            // Return magiclink file
+            if ( $args[0] == 'magiclink_get' ) {
+                if ( isset( $args[1] ) ) {
+                    $username = preg_replace( "/[^a-zA-Z0-9-_]+/", "", $args[1] ); // Sanitized username
+                }
+                if ( isset( $args[2] ) ) {
+                    $domain = preg_replace( "/[^a-zA-Z0-9-_.]+/", "", $args[2] ); // Allow periods in domain
+                }
+    
+                if ( !file_exists( $this->magiclink_folder ) ) {
+                    mkdir( $this->magiclink_folder, 0700, true );
+                }
+                $magiclink_file = "{$this->magiclink_folder}/{$username}_{$domain}";
+                if ( file_exists( $magiclink_file ) ) {
+                    echo file_get_contents( $magiclink_file );
+                } else {
+                    echo "";
+                }
+            }
+            return $args;
+        }
     }
     global $hcpp;
     $hcpp->register_plugin( MagicLink::class );
